6d6e7524a1f84c6ef432c4e39ce8db72897230e0
Nico-Valle <nicovalle092@gmail.com>
2026-04-15 19:22:59 +0200

    Delete include directory

--- include/card-guardioes/script.js
+++
@@ -1,9 +0,0 @@
-/*$(".caixa__imagem__fundo").click(function () {
-  $(".caixa__imagem__fundo").removeClass("caixa__ativo");
-  $(this).addClass("caixa__ativo");
-});*/
-
-$(".caixa__imagem__fundo").click(function () {
-  $(this).closest(".caixa__geral").find(".caixa__imagem__fundo").removeClass("caixa__ativo");
-  $(this).addClass("caixa__ativo");
-});
